c8924e85c82e7d12b2967e4cea3ffea70864cf98
canhbkhn <canhbk2003@gmail.com>
2021-12-09 13:16:12 +0700

    mvc model apply

---
+++ controllers/product.controller.js
@@ -0,0 +1,12 @@
+const db = require('../db');
+const Product = require('../models/product');
+
+class ProductController {
+    getProductPage(req, res, next) {
+
+    }
+
+
+}
+
+module.exports = new ProductController();
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const db = require('../db');
 const User = require('../models/user');
 
 class UserController {
+
     getAdminPage(req, res, next) {
         db.QueryAllUser().then(data => res.render('admin', {
             data: data
@@ -9,6 +10,10 @@ class UserController {
     }
 
     getAddUser(req, res, next) {
+        res.render('adduser');
+    }
+
+    postAddUser(req, res, next) {
         try {
             db.AddOneUser(req.body);
         } catch (err) {
@@ -18,23 +23,44 @@ class UserController {
         res.render('adduser');
     }
 
-    postAddUser(req, res, next) {
-
+    getEditUser(req, res, next) {
+        User.findById(req.params.id)
+            .then(data => res.render('edituser', {
+                user: data
+            }))
+            .catch(next)
     }
 
     putEditUser(req, res, next) {
-
+        User.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/admin'))
+            .catch(next)
     }
 
     getChangePassword(req, res, next) {
-
+        res.render('changepassword');
     }
 
     putChangePassword(req, res, next) {
-
+        // T.B.D
     }
 
     deleteUser(req, res, next) {
+            User.deleteOne({ _id: req.params.id })
+                .then(() => res.render('back'))
+                .catch(next)
+        }
+        // sign up
+    getSignup(req, res, next) {
+        res.render('signup');
+    }
+
+    postSignup(req, res, next) {}
+        // reset password
+    getResetPasswordPage(req, res, next) {
+        res.render('reset');
+    }
+    postResetPasswordPage(req, res, next) {
 
     }
 }
