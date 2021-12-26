09da4415eb882e5b48b7e8acc8231473e98bba46
canhbkhn <canhbk2003@gmail.com>
2021-12-27 06:51:13 +0700

    update ui

--- routes/route.newarrival.js
+++ routes/route.newarrival.js
@@ -5,38 +5,38 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 router.get('/newarrivals', (req, res, next) => {
-  var userName = "Đăng nhập";
-        if (authMiddleware.requireAuth) {
-            const userId = req.cookies.userId;
-            db.GetById(req.cookies.userId).then(data => {
-                if (data) {
-                    const user = new User(data);
-                    userName = user.name;
-                }
-            });
-        }
+  let userName = "Login";
+  const userId = req.cookies.userId;
+  if(userId !== ''){
+    db.GetById(req.cookies.userId).then(data => {
+      if (data) {
+          const user = new User(data);
+          userName = user.name;
+      }
+    });
+  }
 
-        // query all data
-        db.QueryAllProduct().then(data_ => {
-            var _numitems = data_.length / 9;
-            if (_numitems < 1) {
-                _numitems = 1;
-            }
-            _numitems = _numitems + 1;
-            var dpData = [];
-            if (data_.length > 0) {
-                dpData.length = 0;
-                for (var i = 0; i < 9; i++) {
-                    if (data_[i] !== undefined) {
-                        dpData.push(data_[i]);
-                    }
-                }
+  // query all data
+  db.QueryAllProduct().then(data_ => {
+      var _numitems = data_.length / 9;
+      if (_numitems < 1) {
+          _numitems = 1;
+      }
+      _numitems = _numitems + 1;
+      var dpData = [];
+      if (data_.length > 0) {
+          dpData.length = 0;
+          for (var i = 0; i < 9; i++) {
+              if (data_[i] !== undefined) {
+                  dpData.push(data_[i]);
+              }
+          }
 
-                res.render('newarrivals', { numItems: parseInt(_numitems), product: dpData, user: userName});
-            } else {
-                res.render('newarrivals', { numItems: parseInt(_numitems), product: {}, user: userName});
-            }
-        });
+          res.render('newarrivals', { numItems: parseInt(_numitems), product: dpData, user: userName});
+      } else {
+          res.render('newarrivals', { numItems: parseInt(_numitems), product: {}, user: userName});
+      }
+  });
 });
 
 module.exports = router;
